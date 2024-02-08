bd133cb2defd1a0c1c63dd4585cb4596fd9edbe5
[alt]legioner9 <legioner9@inbox.ru>
2024-02-08 22:51:12 +0700

    <> cr .daer.zip #2

--- .d/.ham/main/main.fn.lst.d/daer.sh
+++ .d/.ham/main/main.fn.lst.d/daer.sh
@@ -3,7 +3,9 @@
 echo -e "${BLUE}--- that file://${REPO_PATH}/${name_repo}/.d/.ham/main/main.fn.lst.d/daer.sh ---${NORMAL}" #sistem info mesage
 echo -e "${HLIGHT}--- zip ${REPO_PATH}/lex/.d/.gelu/.daer ${REPO_PATH}/lex/.d/.read ---${NORMAL}" #start files
 
-if ! zip  ${REPO_PATH}/lex/.d/.gelu/.daer ${REPO_PATH}/lex/.d/.read ; then
+cd ${REPO_PATH}/lex/.d/.gelu || plt_exit
+
+if ! pzip_to_ 0 ${REPO_PATH}/lex/.d/.gelu/.daer.zip ${REPO_PATH}/lex/.d/.read 0 ; then
     plt_info "in file://${REPO_PATH}/${name_repo}/.d/.ham/main/main.fn.lst.d/daer.sh : FAIL_EXEC : zip ${REPO_PATH}/lex/.d/.gelu/.daer ${REPO_PATH}/lex/.d/.read : return 1"
     return 1
 fi
