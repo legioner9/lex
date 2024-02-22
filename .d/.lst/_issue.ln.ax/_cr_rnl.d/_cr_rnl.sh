45d21e5cc636fc362a87c54927d9cb5b71c82c7b
[alt]legioner9 <legioner9@inbox.ru>
2024-02-22 19:01:29 +0700

    <> done rln #2

---
+++ .d/.lst/_issue.ln.ax/_cr_rnl.d/_cr_rnl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# rln__ /home/st/REPOBARE/_repo/lex/.d/.lst/_issue.ln.ax/all_known_arb.lst \
+# /home/st/REPOBARE/_repo/lex/.d/.lst/garb_/set_arb \
+# 001_all_known.lst
+
+rln__ /home/st/REPOBARE/_repo/lex/.d/.lst/_issue.ln.ax/all_known_arb.lst \
+/home/st/REPOBARE/_repo/lex/.d/.lst/aE_ \
+all_arb_a.lst
