5e6dd86f840c70ef770305439ff901f69641860d
[alt]legioner9 <legioner9@inbox.ru>
2024-03-02 15:26:34 +0700

    <> ##

--- .d/.arb/proc.arb/tmux_102_php_1.ram/.grot/php_i/index.php
+++ .d/.arb/proc.arb/tmux_102_php_1.ram/.grot/php_i/index.php
@@ -2,5 +2,5 @@
 
 $val=2;
 
-echo " XXXxx that tmux_102_php_1.ram/.grot/php_i/index.php";
+echo " XXXxxRt that tmux_102_php_1.ram/.grot/php_i/index.php";
 echo "2";
---
+++ .d/.arb/proc.arb/tmux_102_php_1.ram/.grot/start_exa.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+arb2mm_ 1 1 2 ${REPO_PATH}/lex/.d/.arb/proc.arb/tmux_102_php_1.ram/.grot/php_i
